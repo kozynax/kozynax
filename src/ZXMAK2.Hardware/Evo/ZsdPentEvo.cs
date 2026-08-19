7170c822388afba18eb26a8e5d0636a8ade19e16
Alexander Tsidaev <a.tsidaev@gmail.com>
2026-08-19 16:14:08 +0500

    SD card: fix NRE on bus disconnect and reset

--- src/ZXMAK2.Hardware/Evo/ZsdPentEvo.cs
+++ src/ZXMAK2.Hardware/Evo/ZsdPentEvo.cs
@@ -76,7 +76,10 @@ namespace ZXMAK2.Hardware.Evo
 
         public override void BusDisconnect()
         {
+            if (card == null)
+                return;
             card.Close();
+            card = null;
         }
 
         #endregion
@@ -87,7 +90,7 @@ namespace ZXMAK2.Hardware.Evo
         protected virtual void Reset()
         {
             card_cs = false;
-            card.Reset();
+            card?.Reset();
             buf = 0xFF;
         }
 
